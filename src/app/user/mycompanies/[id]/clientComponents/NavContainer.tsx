dc23970f31128c0dd7f680084afca59cc59ff914
anush12345 <75567234+anush12345@users.noreply.github.com>
2025-04-07 23:38:43 +0530

    fix: update NavContainer to include params in dependency array

--- src/app/user/mycompanies/[id]/clientComponents/NavContainer.tsx
+++ src/app/user/mycompanies/[id]/clientComponents/NavContainer.tsx
@@ -51,7 +51,7 @@ const NavContainer = ({
     } else {
       setSelected("quick");
     }
-  }, [searchParams]);
+  }, [searchParams, params]);
   return (
     <Box sx={{ display: "flex" }}>
       <CompanySideBar
