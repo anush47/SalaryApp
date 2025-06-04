06fe4a2d4a54b8396ecad3472f3acc9f76338c8c
anush12345 <75567234+anush12345@users.noreply.github.com>
2025-06-05 03:54:44 +0530

    feat: Enhance demo content loading experience with a loading spinner

--- src/app/help/DemoContent.tsx
+++ src/app/help/DemoContent.tsx
@@ -1,6 +1,10 @@
-import React from "react";
+"use client";
+import { CircularProgress } from "@mui/material";
+import React, { useState } from "react";
 
 export const DemoContent = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
   return (
     <div
       style={{
@@ -10,21 +14,31 @@ export const DemoContent = () => {
         width: "100%",
         aspectRatio: "2.183472327520849",
         padding: "40px 0 40px 0",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
       }}
     >
+      {isLoading && (
+        <div>
+          <CircularProgress size="3rem" />
+        </div>
+      )}
       <iframe
         src="https://app.supademo.com/embed/cmbgruw8s51umsn1rde14rujz?embed_v=2"
         loading="lazy"
         title="SalaryApp Demo"
         allow="clipboard-write"
-        frameBorder="0"
         allowFullScreen
+        onLoad={() => setIsLoading(false)}
         style={{
           position: "absolute",
           top: 0,
           left: 0,
           width: "100%",
           height: "100%",
+          opacity: isLoading ? 0 : 1,
+          transition: "opacity 0.5s ease-in-out",
         }}
       ></iframe>
     </div>
